4922c497f67ff91a2f2b3cd8105f67923466fc33
finejy <yoon2926@khu.ac.kr>
2021-03-14 19:02:43 +0900

    start studying opencv.

---
+++ CMakeFiles/startOpencv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/startOpencv.dir/startOpencv.cpp.o"
+  "startOpencv"
+  "startOpencv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/startOpencv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
